c998a83399d6f374094417712401c1a2f55105d5
christopher@goodpairdays.com <christopher@goodpairdays.com>
2025-01-22 10:02:46 +0700

    will clean up later

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/cnohall/Desktop/Programming/blockonomics-api-documentation/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/cnohall/Desktop/Programming/blockonomics-api-documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/cnohall/Desktop/Programming/blockonomics-api-documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/cnohall/Desktop/Programming/blockonomics-api-documentation/src/css/custom.css"),
+];
