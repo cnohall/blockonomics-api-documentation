c5ca480d7e2a002b556dab344018cebb807e37d1
cnohall <christopher.nohall@gmail.com>
2025-01-22 14:34:48 +0700

    remove package-lock.json, add yarn.lock

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require("/Users/cnohall/Desktop/Programming/blockonomics-api-documentation/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/cnohall/Desktop/Programming/blockonomics-api-documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/cnohall/Desktop/Programming/blockonomics-api-documentation/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/cnohall/Desktop/Programming/blockonomics-api-documentation/src/css/custom.css"),
+  require("D:\\Programming\\blockonomics-api-documentation\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("D:\\Programming\\blockonomics-api-documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("D:\\Programming\\blockonomics-api-documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("D:\\Programming\\blockonomics-api-documentation\\src\\css\\custom.css"),
 ];
