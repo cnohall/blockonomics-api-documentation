d661184b08a35461c0a2782cb28b10f661aa6834
cnohall <christopher.nohall@gmail.com>
2025-01-24 14:42:10 +0700

    fix build issue

--- src/components/organisms/playground/index.tsx
+++ src/components/organisms/playground/index.tsx
@@ -1,19 +1,24 @@
+import React, { useState, useEffect } from "react";
+import BrowserOnly from "@docusaurus/BrowserOnly"; // Import BrowserOnly component
 import {
   API_ENDPOINTS,
   LOCAL_STORAGE_API_KEY,
 } from "@site/src/helpers/constants";
-import React, { useState } from "react";
 
 export default function Playground() {
-  const [apiKey, setApiKey] = useState<string>(() => {
-    return localStorage.getItem(LOCAL_STORAGE_API_KEY) || "";
-  });
+  const [apiKey, setApiKey] = useState<string>("");
   const [apiEndpoint, setApiEndpoint] = useState<string>(API_ENDPOINTS[0].url);
   const [apiMethod, setApiMethod] = useState<string>(API_ENDPOINTS[0].method);
+
+  useEffect(() => {
+    // This effect will run only on the client side
+    setApiKey(localStorage.getItem(LOCAL_STORAGE_API_KEY) || "");
+  }, []);
 
   const saveApiKeyToLocalStorage = async () => {
     localStorage.setItem(LOCAL_STORAGE_API_KEY, apiKey);
   };
+
   const clearApiKeyFromLocalStorage = async () => {
     localStorage.setItem(LOCAL_STORAGE_API_KEY, "");
     setApiKey("");
@@ -37,27 +42,33 @@ export default function Playground() {
   };
 
   return (
-    <div>
-      <h3>API Key</h3>
-      <input
-        placeholder="API Key"
-        value={apiKey}
-        onChange={(e) => setApiKey(e.target.value)}
-      />
-      <button onClick={saveApiKeyToLocalStorage}>Save API Key to Cache</button>
-      <button onClick={clearApiKeyFromLocalStorage}>
-        Clear Api Key from Cache
-      </button>
-      {/* Add a dropdown for different API endpoints */}
-      <h3>API Endpoints</h3>
-
-      <p>{apiEndpoint}</p>
-      <select onChange={(e) => setApiEndpointData(e.target)}>
-        {Object.values(API_ENDPOINTS).map((endpoint) => (
-          <option value={endpoint.url}>{endpoint.name}</option>
-        ))}
-      </select>
-      <button>Send Request</button>
-    </div>
+    <BrowserOnly fallback={<div>Loading...</div>}>
+      {() => (
+        <div>
+          <h3>API Key</h3>
+          <input
+            placeholder="API Key"
+            value={apiKey}
+            onChange={(e) => setApiKey(e.target.value)}
+          />
+          <button onClick={saveApiKeyToLocalStorage}>
+            Save API Key to Cache
+          </button>
+          <button onClick={clearApiKeyFromLocalStorage}>
+            Clear API Key from Cache
+          </button>
+          <h3>API Endpoints</h3>
+          <p>{apiEndpoint}</p>
+          <select onChange={(e) => setApiEndpointData(e.target)}>
+            {Object.values(API_ENDPOINTS).map((endpoint) => (
+              <option key={endpoint.url} value={endpoint.url}>
+                {endpoint.name}
+              </option>
+            ))}
+          </select>
+          <button onClick={sendRequest}>Send Request</button>
+        </div>
+      )}
+    </BrowserOnly>
   );
 }
