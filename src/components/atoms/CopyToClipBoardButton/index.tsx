a7fdfb9994b5b7baad7e0075807be278ed5c607c
cnohall <christopher.nohall@gmail.com>
2025-02-03 11:32:21 +0700

    fix dark mode copy icon styles

--- src/components/atoms/CopyToClipBoardButton/index.tsx
+++ src/components/atoms/CopyToClipBoardButton/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { FiClipboard, FiCheck } from "react-icons/fi";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./styles.css";
+import { useColorMode } from "@docusaurus/theme-common";
+import { DOCUSAURUS_COLOR_MODES } from "@site/src/helpers/constants";
 
 const CopyToClipboardButton = ({ content, hasMouseEnteredCodeBlock }) => {
   const [isCopied, setIsCopied] = React.useState(false);
+  const { colorMode } = useColorMode();
+  const isDarkMode = colorMode === DOCUSAURUS_COLOR_MODES.DARK;
 
   const handleCopy = () => {
     navigator.clipboard.writeText(content);
@@ -29,6 +33,7 @@ const CopyToClipboardButton = ({ content, hasMouseEnteredCodeBlock }) => {
             className="copy-icon"
             style={{
               opacity: hasMouseEnteredCodeBlock ? 0.5 : 0,
+              color: isDarkMode ? "white" : "black",
             }}
             onClick={handleCopy}
           />
