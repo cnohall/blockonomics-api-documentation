cf448cb109e6d714d468936f161e1c3c09372ef3
cnohall <christopher.nohall@gmail.com>
2025-02-07 11:41:31 +0900

    temporarily test swagger UI

---
+++ src/components/organisms/SwaggerTest/index.tsx
@@ -0,0 +1,8 @@
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+
+const SwaggerTest = () => (
+  <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+);
+
+export default SwaggerTest;
